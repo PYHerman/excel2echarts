2439ab447dc5e31afb9cd625dee6a4c7970b398c
PYHerman <731207730@qq.com>
2019-07-16 13:52:00 +0800

    'fixed_toolbox'

--- src/components/linechart.vue
+++ src/components/linechart.vue
@@ -138,6 +138,7 @@
             initChart() {
                 this.chart = echarts.init(document.getElementById(this.id));
                 //chart自适应变化
+                var that = this;
                 window.onresize = this.chart.resize;
 
 
@@ -163,49 +164,24 @@
                             //-------------------------------------------------------------------------------------------------
                             myTool1: {
                                 show: true,
-                                title: '显示/隐藏标签',
+                                title: '标签显示',
                                 icon: 'path://M432.45,595.444c0,2.177-4.661,6.82-11.305,6.82c-6.475,0-11.306-4.567-11.306-6.82s4.852-6.812,11.306-6.812C427.841,588.632,432.452,593.191,432.45,595.444L432.45,595.444z M421.155,589.876c-3.009,0-5.448,2.495-5.448,5.572s2.439,5.572,5.448,5.572c3.01,0,5.449-2.495,5.449-5.572C426.604,592.371,424.165,589.876,421.155,589.876L421.155,589.876z M421.146,591.891c-1.916,0-3.47,1.589-3.47,3.549c0,1.959,1.554,3.548,3.47,3.548s3.469-1.589,3.469-3.548C424.614,593.479,423.062,591.891,421.146,591.891L421.146,591.891zM421.146,591.891',
-                                onclick: function (a, b, c, d) {
-                                    console.log(a)
-                                    console.log(b)
-                                    console.log(c)
-                                    console.log(d)
-                                    var chart = document.getElementById('chart');
+                                onclick: function(a, obj, c, d) {
+                                    var op = obj.getOption();
 
-                                    for (var key in a.option.series) {
-
-
-
-
-                                        a.option.series[key].label.show = true;
+                                    var tmpSerise = [];
+                                    for (var i = 0; i < op.series.length; i++) {
+                                        op.series[i].label.show = !op.series[i].label.show;
+                                        tmpSerise.push(op.series[i]);
                                     }
-
-                                    $(d.target).dispatchAction(a);
-
-
-                                    console.log(a)
+                                    that.chart.setOption({
+                                        series:tmpSerise
+                                    });
                                 }
-                                /*onclick: (option1)=>this.chart.setOption({
-                                    series:[{label:{show:!option1.option.series[0].label.show}},{label:{show:!option1.option.series[1].label.show}},{label:{show:!option1.option.series[2].label.show}},{label:{show:!option1.option.series[3].label.show}},]
-                                }),//先用这种方法凑合，下面的还没写好*/
+
                             },
 
-                            // onclick:function(option1){
-                            //     var newOption=option1.option.series;
-                            //
-                            //     //console.log(newOption);--------------------------------------------------------------待完成
-                            //     for(var key in newOption)
-                            //     {
-                            //         newOption[key].label.show=!newOption[key].label.show;
-                            //
-                            //     }
-                            //     option1.option.series=newOption;
-                            //     var chart = document.getElementById('chart');
-                            //     //chart.setOption(option1);
-                            //     console.log(chart);
-                            //     return option1;
-                            //
-                            // }
+
 
                         }
                     },
